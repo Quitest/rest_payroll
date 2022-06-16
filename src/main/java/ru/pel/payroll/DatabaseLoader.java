41e855a943ae956511c093ecda5e65538b08c461
Quitest <Quitest>
2022-06-16 22:27:47 +0300

    Инициализация переписана с учетом новой сущности - Order.

--- src/main/java/ru/pel/payroll/DatabaseLoader.java
+++ src/main/java/ru/pel/payroll/DatabaseLoader.java
@@ -12,10 +12,15 @@ public class DatabaseLoader {
     private static final Logger LOG = LoggerFactory.getLogger(DatabaseLoader.class);
 
     @Bean
-    public CommandLineRunner initDB(EmployeeRepository repository){
+    public CommandLineRunner initDB(EmployeeRepository employeeRepository, OrderRepository orderRepository){
         return args -> {
-            LOG.info("Preloading "+repository.save(new Employee("Pit", "Smith", "staff")));
-            LOG.info("Preloading "+repository.save(new Employee("Max", "Green", "director")));
+            employeeRepository.save(new Employee("Pit", "Smith", "staff"));
+            employeeRepository.save(new Employee("Max", "Green", "director"));
+            employeeRepository.findAll().forEach(employee->LOG.info("Preloaded -> {}", employee));
+
+            orderRepository.save(new Order("MacBook", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+            orderRepository.findAll().forEach(order -> LOG.info("Preloaded -> {}", order));
         };
     }
 }
