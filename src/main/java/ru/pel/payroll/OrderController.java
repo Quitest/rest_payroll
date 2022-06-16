149ccab1df862516dfd8d1bd0c57bee0fb37d5f0
Quitest <Quitest>
2022-06-16 22:26:30 +0300

    Реализован метод complete

--- src/main/java/ru/pel/payroll/OrderController.java
+++ src/main/java/ru/pel/payroll/OrderController.java
@@ -2,12 +2,10 @@ package ru.pel.payroll;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +16,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class OrderController {
-    private final OrderRepository orderRepository;
+    private final OrderRepository repository;
     private final OrderModelAssembler orderAssembler;
 
     public OrderController(OrderRepository orderRepository, OrderModelAssembler orderAssembler) {
-        this.orderRepository = orderRepository;
+        this.repository = orderRepository;
         this.orderAssembler = orderAssembler;
     }
 
     @GetMapping("/orders")
     public CollectionModel<EntityModel<Order>> all() {
-        List<EntityModel<Order>> entityModels = orderRepository.findAll().stream()
+        List<EntityModel<Order>> entityModels = repository.findAll().stream()
                 .map(orderAssembler::toModel)
                 .toList();
         return CollectionModel.of(entityModels,
@@ -37,14 +35,14 @@ public class OrderController {
 
     @GetMapping("/orders/{id}")
     public EntityModel<Order> one(@PathVariable long id) {
-        Order order = orderRepository.findById(id).orElseThrow(); //TODO добавить кастомный эксепшен
+        Order order = repository.findById(id).orElseThrow(); //TODO добавить кастомный эксепшен
         return orderAssembler.toModel(order);
     }
 
     @PostMapping("/orders")
     public ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order newOrder){
         newOrder.setStatus(Status.IN_PROGRESS);
-        Order order = orderRepository.save(newOrder);
+        Order order = repository.save(newOrder);
         EntityModel<Order> orderEntityModel = orderAssembler.toModel(order);
         return ResponseEntity
 //                .created(orderEntityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //TODO сравнить с нижним вариантом
@@ -54,10 +52,10 @@ public class OrderController {
 
     @DeleteMapping("/orders/{id}/cancel")
     public ResponseEntity<?> cancel(@PathVariable long id){
-        Order order = orderRepository.findById(id).orElseThrow(); //TODO добавить кастомный эксепшен
+        Order order = repository.findById(id).orElseThrow(); //TODO добавить кастомный эксепшен
         if (order.getStatus() == Status.IN_PROGRESS){
             order.setStatus(Status.CANCELLED);
-            return ResponseEntity.ok(orderAssembler.toModel(orderRepository.save(order)));
+            return ResponseEntity.ok(orderAssembler.toModel(repository.save(order)));
         }
 
         return ResponseEntity
@@ -67,4 +65,23 @@ public class OrderController {
                         .withTitle("Method not allowed")
                         .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
     }
+
+    @PutMapping("/orders/{id}/complete")
+    public ResponseEntity<?> complete(@PathVariable long id){
+        Order order = repository.findById(id).orElseThrow(); //TODO сделать кастомное исалючение
+
+        if (order.getStatus() == Status.IN_PROGRESS){
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(orderAssembler.toModel(order));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed")
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
+    }
+
+
 }
